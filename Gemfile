6ceb42b5f7f824fd1ac9d9f72c7b8532f92948e6
Wolfgang Barth <wob@swobspace.net>
2016-05-01 13:46:18 +0200

    initial checkin

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "rb-inotify"
+gem "actionmailer", '4.2.5'
+gem "dotenv"
---
+++ tail.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'yaml'
+require 'rb-inotify'
+require 'action_mailer'
+require 'dotenv'
+Dotenv.load
+
+logfile = ARGV.first || '/var/log/messages'
+
+# ---------------------------------------------------------
+# Mailer
+# ---------------------------------------------------------
+
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.view_paths= File.dirname(__FILE__)
+
+class Mailer < ActionMailer::Base
+  default from: ENV['MAIL_FROM'], to: ENV['MAIL_TO']
+
+  def boot
+    mail(subject: "Starting syslogeve2mail ...")
+  end
+
+  def alert_mail(ids)
+    @ids = ids
+    @alrt = ids['alert']
+    subject  = "#{ids['sensor']}:[#{@alrt['gid']}:#{@alrt['signature_id']}:#{@alrt['rev']}]"
+    subject += " #{@alrt['signature']}/#{@alrt['category']}/#{@alrt['severity']}: "
+    subject += "#{ids['proto']} #{ids['src_ip']}:#{ids['src_port']} -> #{ids['dest_ip']}:#{ids['dest_port']}"
+
+    mail(subject: subject)
+  end
+end
+
+Mailer.boot.deliver_now
+
+# ---------------------------------------------------------
+# Main
+# ---------------------------------------------------------
+
+open(logfile, 'r') do |file|
+  file.seek(0, IO::SEEK_END)		# rewind
+  queue = INotify::Notifier.new
+  queue.watch(logfile, :modify) do |event|
+    file.each_line do |line|
+      next unless line =~ /suricata.*"timestamp"/
+      m = line.match(/\A([A-Za-z]{3} [ 0-9]{2} \d\d:\d\d:\d\d) (\w+) suricata\[\d+\]: (\{.+\})/)
+      hash = JSON.parse(m[3])
+      hash['sensor'] = m[2]
+      email = Mailer.alert_mail(hash)
+      puts email
+      email.deliver_now
+    end
+  end
+  queue.run
+end
